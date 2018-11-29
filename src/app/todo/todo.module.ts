20cd95b5759323255bfe25eba311f5ecca862301
EsraaAtefibrahim <nesrin.abdulaziz14@gmail.com>
2018-11-29 12:29:02 +0200

    added todo routing

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoComponent } from './todo/todo.component';
+import { MatFormField, MatFormFieldModule, MAT_DATE_LOCALE_PROVIDER, 
+  MatDatepicker, MatDatepickerModule, MatNativeDateModule, _MatInputMixinBase, MatInputModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule
   ],
   declarations: [TodoComponent]
 })
--- src/app/todo/todo/todo.component.ts
+++ src/app/todo/todo/todo.component.ts
@@ -12,7 +12,7 @@ export class TodoComponent implements OnInit {
   constructor() { }
   
   ngOnInit() {
-
+    
   }
 
 }
