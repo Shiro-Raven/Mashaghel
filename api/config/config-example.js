8e2648248cf33dd4645a3a857d20c96067fdeefd
JARVIS\Sinclair <exclass9.24@gmail.com>
2018-12-03 21:40:27 +0200

    feat: added example config and edited README

---
+++ api/config/config-example.js
@@ -0,0 +1,7 @@
+/*eslint-disable */
+module.exports = {
+  EMAIL: 'example@yahoo.com',
+  FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:4200',
+  MONGO_URI: process.env.DB_URL || 'mongodb://localhost:27017/sample-db',
+  API_SECRET: process.env.API_SECRET || ';iN.yVt,Tmu44cZkX#.|tS>s`4xb;-oRe66iMz0[L^e9;ltF_5"DUvPphj:f:&',
+};
